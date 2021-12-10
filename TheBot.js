5027fe2c35ba1c43f13b91d84ebca6392aa58eda
Ranvir Singh Virk <49649026+Ranvirsv@users.noreply.github.com>
2021-12-09 23:19:49 -0500

    Update TheBot.js

--- TheBot.js
+++ TheBot.js
@@ -25,21 +25,59 @@ client.on('ready', () => {
 
     setInterval(() => {
         const random = statusList[Math.floor(Math.random(statusList.length) * statusList.length)].split(', ');
-        // chosses a random status form the status list and splits it into two words, the status and the type.
         const status = random[1];
         const mode = random[0];
         client.user.setActivity(status, {type: mode});
-    }, 60000)
+    }, 3.6e+6)
 });
 
-client.on('messageCreate', (msg) => {
+function callHelp(){
+    msg.channel.send('# rec/keyword - for recommending movies and tv shows from a specific keyword');
+    msg.channel.send('- Keywords include Love, Laughter, Ghost, Dramas, etc')
+    msg.channel.send('# rec250/Movies - for recommending movies from top 250 imdb movie list');
+    msg.channel.send('# rec250/TVs - for recommending tv shows from top 250 imdb series list');
+}
 
+function recRandom(list, msg, res) {
+    const index = Math.floor(Math.random() * list.length);
+    // choses a random number between 0 and the length of the items in the data of the responce we got.
+    const reply = list[index];
+    
+    msg.channel.send(reply.title);
+    
+    if(res.data.items[index].image === ''){
+        msg.channel.send({
+            content: 'No image found'
+        });
+    }
+    else {
+        msg.channel.send(reply.image);
+    }
+    
+    msg.channel.send({
+        content: `Imdb rating: ${reply.imDbRating}`
+    });
+}
+
+function makeList(list){
+    let requiredList = [];
+    let k = 0;
+    for (let i = 0; i < list.length; i++) {
+        if(list[i].imDbRating >= 7.0){
+            requiredList[k] = list[i];
+            k++;
+        }
+    }
+
+    return requiredList;
+}
+
+client.on('messageCreate', (msg) => {
+ 
     let command = msg.content.split('/');
     // the the command message into two words using the split command to be processed differently.
-   
+
     console.log(command);
-    console.log(command[0]);
-    console.log(command[1]);
 
     if(msg.content === '/ping'){
         msg.reply({
@@ -53,6 +91,9 @@ client.on('messageCreate', (msg) => {
     }
     else if(command[0] === 'rec'){
 
+        console.log(command[0]);
+        console.log(command[1]);
+
         // make a free account on imdb api and get the api key
         // then put the key in the env file and use it.
         axios.get(`https://imdb-api.com/en/API/Keyword/${process.env.APIKEY}/${command[1]}`)
@@ -61,37 +102,28 @@ client.on('messageCreate', (msg) => {
         // the keywords can be love, scary, ghost, dramas etc.
         .then((res) => {
 
-            if(res.data.items.length > 0){
-                const index = Math.floor(Math.random() * res.data.items.length);
-                // choses a random number between 0 and the length of the items in the data of the responce we got.
-                const reply = res.data.items[index];
-                
-                msg.channel.send(reply.title);
-                
-                if(res.data.items[index].image === ''){
-                    msg.channel.send({
-                        content: 'No image found'
-                    });
-                }
-                else {
-                    msg.channel.send(reply.image);
-                }
 
-                msg.channel.send({
-                    content: `Imdb rating: ${reply.imDbRating}`
-                });
+            console.log('RES', res.data.items);
+
+            const updatedList = makeList(res.data.items);
+
+            if(res.data.items.length === 0){
+                msg.channel.send('Dahi is called curd, try another keyword.');
+            }
+
+            else if(updatedList.length > 0){
+                recRandom(updatedList, msg, res);
             }
             else{
-                msg.channel.send('Keyword not accepted, try something else.')
+                msg.channel.send('We don\'t recommend shit media. Try a different keyword');
             }
-            console.log('RES', reply);
         })
         .catch((err) => {
             console.log('ERR', err);
         });
     }
     else if(command[0] === 'rec250'){
-        axios.get(`https://imdb-api.com/en/API/Top250${command[1]}/${process.env.APIKEY}`)
+        axios.get(`https://imdb-api.com/en/API/Top250${command[1]}/k_eggvdssr`)
         // gets the specific media form the imdb data base, i.e movies or series.
         .then((res) => {
             const index = Math.floor(Math.random() * res.data.items.length)
@@ -110,10 +142,6 @@ client.on('messageCreate', (msg) => {
         });
     }
     else if(command[1] === 'rec-help'){
-        msg.channel.send('rec/keyword - for recommending movies and tv shows from a specific keyword');
-        msg.channel.send('            - Keywords include Love, Laughter, Ghost, Dramas, etc')
-        msg.channel.send('rec250/Movies - for recommending movies from top 250 imdb movie list');
-        msg.channel.send('rec250/TVs - for recommending tv shows from top 250 imdb series list');
+        callHelp();
     }
-    // this command, when used, helps you by presenting all the commands that you can use of the bot.
 });
